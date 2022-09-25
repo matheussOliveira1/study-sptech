8b0e88a92954de6cf3a8efeade43521725f554aa
matheussOliveira1 <matheus.aoliveira@sptech.school>
2022-09-25 03:46:10 -0300

    Interface de login e pós login adicionadas e login conectado ao banco de dados, funcionando corretamente

---
+++ study-looca/src/main/java/school/sptech/api/looca/InserirDados.java
@@ -0,0 +1,30 @@
+package school.sptech.api.looca;
+
+import com.github.britooo.looca.api.core.Looca;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InserirDados {
+
+    static final String DB_URL = "jdbc:mysql://localhost/dataSentry";
+    static final String USER = "root";
+    static final String PASS = "matheus123";
+
+    public void inserirBd() {
+
+        Looca looca = new Looca();
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                Statement stmt = conn.createStatement();) {
+            // INSERIR NO BANCO DE DADOS
+            System.out.println("\nTESTANDO INSERÇÃO");
+            String sql = "INSERT INTO Usuario VALUES (INSERIR OS VALORES)";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ study-looca/src/main/java/school/sptech/api/looca/PegarDadosLogin.java
@@ -0,0 +1,78 @@
+package school.sptech.api.looca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PegarDadosLogin {
+
+    static final String DB_URL = "jdbc:mysql://localhost/dataSentry";
+    static final String USER = "root";
+    static final String PASS = "matheus123";
+    static final String QUERY = "SELECT email, senha FROM Usuario";
+
+    public List<String> getEmails() {
+
+        String emailLogin;
+        List<String> emails = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(QUERY);) {
+            // PEGAR VALORES DO BANCO DE DADOS
+            while (rs.next()) {
+                // Retrieve by column name
+                emailLogin = rs.getString("Email");
+                emails.add(emailLogin);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return emails;
+    }
+
+    public List<String> getSenhas() {
+
+        String senhaLogin;
+        List<String> senhas = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(QUERY);) {
+
+            // PEGAR VALORES DO BANCO DE DADOS
+            while (rs.next()) {
+                // Retrieve by column name
+                senhaLogin = rs.getString("Senha");
+                senhas.add(senhaLogin);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return senhas;
+    }
+
+    public Integer getTamanhoLista() {
+
+        Integer tamanhoLista = 0;
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(QUERY);) {
+
+            // PEGAR VALORES DO BANCO DE DADOS
+            while (rs.next()) {
+                // Retrieve by column name
+                rs.getString("Email");
+                tamanhoLista++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return tamanhoLista;
+    }
+}
